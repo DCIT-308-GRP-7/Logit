eda9507953ef5961e38f5fd7ac1cd93321a59438
rmartey <marteyrichmond001@gmail.com>
2022-09-01 13:43:06 +0000

    changed db

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hel  lo World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
