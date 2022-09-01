cd266db3614dfb70a367d00bd3907c0d6917e56d
Prince Jan Kumi <janprince002@gmail.com>
2022-09-01 00:25:36 +0000

    Add add cat

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import classes.Product;
-import classes.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,14 +11,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import sample.DBTest;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Goods implements Initializable {
+public class ProductController implements Initializable {
     @FXML private TableView<Product> tableView;
     @FXML private TableColumn<Product, Integer> idColumn;
     @FXML private TableColumn<Product, String> nameColumn;
@@ -29,7 +30,22 @@ public class Goods implements Initializable {
     @FXML private TableColumn<Product, Float> grossPriceColumn;
 
 
-//    public void loginClicked(ActionEvent actionEvent) throws Exception {
+//    public void addProduct(MouseEvent actionEvent){
+//        Parent root;
+//        try {
+//            root = FXMLLoader.load(getClass().getClassLoader().getResource("../fxml/addproduct.fxml"));
+//            Stage stage = new Stage();
+//            stage.setTitle("My New Stage Title");
+//            stage.setScene(new Scene(root, 450, 450));
+//            stage.show();
+//            // Hide this current window (if this is what you want)
+////            ((Node)(event.getSource())).getScene().getWindow().hide();
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
 //        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
 //
 //        Parent root = FXMLLoader.load(getClass().getResource("../fxml/vendors.fxml"));
@@ -50,5 +66,21 @@ public class Goods implements Initializable {
         // load data
         tableView.setItems(DBTest.getGoods());
 
+    }
+
+    public void addProduct(MouseEvent actionEvent) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../fxml/addproduct.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("My New Stage Title");
+            stage.setScene(new Scene(root));
+            stage.show();
+            // Hide this current window (if this is what you want)
+//            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
